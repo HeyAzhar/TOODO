5647839cc967d2efd6582d364c53e25c23b35655
HeyAzhar <sk.azharuddin1230@gmail.com>
2020-05-04 04:38:12 +0530

    Added removing and and reseting animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
-  $('h1').hide().slideDown(700);
-  $('section').hide().fadeIn(700);
+  $('h1').hide().slideDown(800);
+  $('section').hide().fadeIn(800);
   var $textInput = $('input:text');
   var $list = $('#list');
   var $addButton = $('#addButton');
@@ -15,21 +15,30 @@ $(function() {
       }, 8);
     } else {
       $list.append('<li><input type="checkbox" id=" ' + $text + ' "><label for=" ' + $text + ' "><i class="fas fa-check"></i>' + $text + '</label></li>');
-
+      
       $textInput.val('');
     };
   });
 
   // reset todo
   $('#reset').on('click', function() {
-    $('li').remove();
+    $('li').animate({
+      opacity: 0.00,
+      paddingLeft: '+=80'
+    }, 500, () => {
+      $('li').remove();
+    })
   });
 
   // remove todo
   $('#remove').on('click', function() {
     var $completed = $('input:checked');
-
-    $completed.parentsUntil('ul').remove();
+    $completed.parentsUntil('ul').animate({
+      opacity: 0.00,
+      paddingLeft: '+=80'
+    }, 500, () => {
+      $completed.parentsUntil('ul').remove();
+    })
   });
 
 })
